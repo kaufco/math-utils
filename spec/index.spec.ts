57a15441e10b767a4c7c5b0b35833f55a670824a
Marco Kaufmann <Marco.Kaufmann@dmtm.com>
2021-04-27 03:18:28 +0200

    added lots of new functions

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,5 +1,25 @@
 // @ts-ignore
-import { clamp, clampUnit, mod, rangeToUnit, uByteToUnit, unitToRange, unitToUByte } from '../src';
+import {
+    clamp,
+    clampUnit,
+    degreesToDefaultRange,
+    degreesToRadians,
+    divFloor,
+    divTrunc,
+    getDigits,
+    log,
+    maskMinus0,
+    mod,
+    radiansToDefaultRange,
+    radiansToDegrees,
+    rangeToRange,
+    rangeToUnit,
+    root,
+    signum,
+    uByteToUnit,
+    unitToRange,
+    unitToUByte,
+} from '../src';
 
 describe('Test Suite', () => {
     it('clamp', () => {
@@ -34,6 +54,17 @@ describe('Test Suite', () => {
         ]) {
             expect(rangeToUnit(value!, startValue!, endValue!)).toEqual(expected!);
             expect(unitToRange(expected!, startValue!, endValue!)).toEqual(value!);
+        }
+    });
+    it('rangeToRange', () => {
+        for (const [value, startValueFrom, endValueFrom, startValueTo, endValueTo, expected] of [
+            [12, 20, 30, 100, 200, 20],
+            [20, 20, 30, 100, 200, 100],
+            [27, 20, 30, 100, 200, 170],
+            [30, 20, 30, 100, 200, 200],
+            [39, 20, 30, 100, 200, 290],
+        ]) {
+            expect(rangeToRange(value!, startValueFrom!, endValueFrom!, startValueTo!, endValueTo!)).toEqual(expected!);
         }
     });
     it('unitToUByte', () => {
@@ -71,30 +102,139 @@ describe('Test Suite', () => {
             [1 / 3, 85],
             [2 / 3, 170],
             [20 / 255, 20],
-            [20 / 255, 20],
+            [20 / 255, 20.4],
+            [20 / 255, 20.5],
+            [20 / 255, 20.6],
             [21 / 255, 21],
-            [21 / 255, 21],
+            [21 / 255, 21.4],
+            [21 / 255, 21.5],
+            [21 / 255, 21.6],
         ]) {
             expect(uByteToUnit(value!)).toEqual(expected!);
         }
     });
-    it('mod', () => {
-        for (const [a, b, expected] of [
-            [-13, 5, 2],
-            [-8, 5, 2],
-            [-3, 5, 2],
-            [2, 5, 2],
-            [7, 5, 2],
-            [-16, 3, 2],
-            [-17, 3, 1],
-            [-18, 3, 0],
-            [-19, 3, 2],
-            [16, 3, 1],
-            [17, 3, 2],
-            [18, 3, 0],
-            [19, 3, 1],
+    it('maskMinus0, signum', () => {
+        for (const [value, expectedMaskMinus0, expectedSignum] of [
+            [-3.4, -3.4, -1],
+            [-3, -3, -1],
+            [-1, -1, -1],
+            [-0.2, -0.2, -1],
+            [-0, 0, 0],
+            [0, 0, 0],
+            [3, 3, 1],
+            [5.2, 5.2, 1],
         ]) {
-            expect(mod(a!, b!)).toEqual(expected!);
+            expect(maskMinus0(value!)).toEqual(expectedMaskMinus0!);
+            expect(signum(value!)).toEqual(expectedSignum!);
+        }
+    });
+    it('mod, divFloor, divTrunc', () => {
+        for (const [a, b, expectedMod, expectedDivFloor, expectedDivTrunc] of [
+            [-13, 5, 2, -3, -2],
+            [-8, 5, 2, -2, -1],
+            [-3, 5, 2, -1, 0],
+            [2, 5, 2, 0, 0],
+            [7, 5, 2, 1, 1],
+            [-16, 3, 2, -6, -5],
+            [-17, 3, 1, -6, -5],
+            [-18, 3, 0, -6, -6],
+            [-19, 3, 2, -7, -6],
+            [16, 3, 1, 5, 5],
+            [17, 3, 2, 5, 5],
+            [18, 3, 0, 6, 6],
+            [19, 3, 1, 6, 6],
+        ]) {
+            expect(mod(a!, b!)).toEqual(expectedMod!);
+            expect(divFloor(a!, b!)).toEqual(expectedDivFloor!);
+            expect(divTrunc(a!, b!)).toEqual(expectedDivTrunc!);
+            expect(b! * divFloor(a!, b!) + mod(a!, b!)).toEqual(a!);
+            expect(b! * divTrunc(a!, b!) + (a! % b!)).toEqual(a!);
+        }
+    });
+    it('degreesToRadians, radiansToDegrees, degreesToDefaultRange, radiansToDefaultRange', () => {
+        for (const [degrees, radians] of [
+            [-0, -0 * Math.PI],
+            [0, 0],
+            [45, 0.25 * Math.PI],
+            [90, 0.5 * Math.PI],
+            [180, Math.PI],
+            [270, 1.5 * Math.PI],
+            [315, 1.75 * Math.PI],
+        ]) {
+            for (let k = -3; k < 4; k++) {
+                const kDegrees = degrees! + k * 360;
+                const kRadians = radians! + k * 2 * Math.PI;
+                expect(degreesToRadians(kDegrees)).toBeCloseTo(kRadians, 12);
+                expect(radiansToDegrees(kRadians)).toBeCloseTo(kDegrees, 12);
+                expect(degreesToDefaultRange(kDegrees)).toBeCloseTo(degrees!, 12);
+                expect(radiansToDefaultRange(kRadians)).toBeCloseTo(radians!, 12);
+            }
+        }
+    });
+    it('log, root', () => {
+        for (const [radix, exponent, value] of [
+            [2, 3, 8],
+            [2, 6, 64],
+            [2, 16, 0x10000],
+            [3, 0, 1],
+            [3, 1, 3],
+            [3, 2, 9],
+            [3, 3, 27],
+            [3, 4, 81],
+            [3, 5, 243],
+            [10, -2, 0.01],
+            [10, -1, 0.1],
+            [10, 0, 1],
+            [10, 1, 10],
+            [10, 2, 100],
+            [10, 3, 1000],
+            [10, 9, 1000000000],
+            [Math.SQRT2, 2, 2],
+            [Math.SQRT2, 10, 32],
+        ]) {
+            expect(log(value!, radix!)).toBeCloseTo(exponent!);
+            if (exponent !== 0) {
+                expect(root(value!, exponent!)).toBeCloseTo(radix!);
+            }
+        }
+    });
+    it('log, root', () => {
+        for (const [radix, value, expected] of [
+            [10, 0, 1],
+            [10, 0.002342, 1],
+            [10, 0.5, 1],
+            [10, 0.9992, 1],
+            [10, 1, 1],
+            [10, 1.000001, 1],
+            [10, 1000001, 7],
+            [10, 5, 1],
+            [10, 9.999, 1],
+            [10, 10.005, 2],
+            [10, 32, 2],
+            [10, 99, 2],
+            [10, 99.999, 2],
+            [10, 10, 2],
+            [10, 100, 3],
+            [10, 1000000000, 9],
+            [16, 0xffff, 4],
+            [16, 0xfff, 3],
+            [16, 0xff, 2],
+            [16, 0xf, 1],
+            [16, 1, 1],
+            [16, 0, 1],
+            [16, 0x10000, 5],
+            [16, 0x1000, 4],
+            [16, 0x100, 3],
+            [16, 0x10, 2],
+            [16, 0x4abe2, 5],
+            [16, 0x9ad3, 4],
+            [16, 0xcf7, 3],
+            [16, 0x7f, 2],
+            [3, 3 * 3 * 3 * 3, 5],
+            [3, 3 * 3 * 3 * 3 + 1, 5],
+            [3, 3 * 3 * 3 * 3 - 1, 4],
+        ]) {
+            expect(getDigits(value!, radix!)).toBe(expected!);
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -19,43 +19,78 @@ export function clampUnit(value: number): number {
 }
 
 /**
- * Converts a value from range _0 .. 1_ (inclusive) to range _startValue .. endValue_ (inclusive).
+ * Converts a value from range _0 .. 1_ (inclusive)
+ * to range _startValueTo .. endValueTo_ (inclusive)
+ * by linear interpolation.
  *
- * @param value value to convert
- * @param startValue start value of the range
- * @param endValue end value of the range
+ * This is equivalent to the [Lerp](//https://en.wikipedia.org/wiki/Linear_interpolation) function,
+ * where _value_ is the interpolation ratio, and the return value is the interpolated value.
+ *
+ * @param value value to convert, or interpolation ratio
+ * @param startValueTo start value of the target range
+ * @param endValueTo end value of the target range
  */
-export function unitToRange(value: number, startValue: number, endValue: number): number {
-    return startValue + value * (endValue - startValue);
+export function unitToRange(value: number, startValueTo: number, endValueTo: number): number {
+    return startValueTo + value * (endValueTo - startValueTo);
 }
 
 /**
- * Converts a value from range _startValue .. endValue_ (inclusive) to range _0 .. 1_ (inclusive).
+ * Converts a value from range _startValueFrom .. endValueFrom_ (inclusive)
+ * to range _0 .. 1_ (inclusive)
+ * by linear interpolation.
  *
- * @param value value to convert
- * @param startValue start value of the range
- * @param endValue end value of the range
+ * This is equivalent to the inverse of the [Lerp](//https://en.wikipedia.org/wiki/Linear_interpolation) function,
+ * where _value_ is the interpolated value, and the return value is the interpolation ratio.
+ *
+ * @param value value to convert, or interpolated value
+ * @param startValueFrom start value of the source range
+ * @param endValueFrom end value of the source range
  */
-export function rangeToUnit(value: number, startValue: number, endValue: number): number {
-    return (value - startValue) / (endValue - startValue);
+export function rangeToUnit(value: number, startValueFrom: number, endValueFrom: number): number {
+    return (value - startValueFrom) / (endValueFrom - startValueFrom);
 }
 
 /**
- * Converts a value from range _0 .. 1_ (inclusive) to an integer value in the range _0 .. 255_ (inclusive).
+ * Converts a value from range _startValueFrom .. endValueFrom_ (inclusive)
+ * to range _startValueTo .. endValueTo_ (inclusive)
+ * by linear interpolation.
  *
- * @param value value to convert
+ * @param value value to convert, or interpolated value
+ * @param startValueFrom start value of the source range
+ * @param endValueFrom end value of the source range
+ * @param startValueTo start value of the target range
+ * @param endValueTo end value of the target range
+ */
+export function rangeToRange(
+    value: number,
+    startValueFrom: number,
+    endValueFrom: number,
+    startValueTo: number,
+    endValueTo: number,
+): number {
+    return unitToRange(rangeToUnit(value, startValueFrom, endValueFrom), startValueTo, endValueTo);
+}
+
+/**
+ * Converts a value from range _0 .. 1_ (inclusive)
+ * to an integer number in the range _0 .. 255_ (inclusive)
+ * by linear interpolation.
+ *
+ * @param value value to convert, or interpolation ratio
  */
 export function unitToUByte(value: number): number {
     return Math.round(unitToRange(value, 0, 255));
 }
 
 /**
- * Converts a value from range _0 .. 1_ (inclusive) to an integer value in the range _0 .. 255_ (inclusive).
+ * Converts an integer number in the range _0 .. 255_ (inclusive)
+ * to range _0 .. 1_ (inclusive)
+ * by linear interpolation.
  *
- * @param value value to convert
+ * @param value integer number to convert, or interpolated value
  */
 export function uByteToUnit(value: number): number {
-    return rangeToUnit(value, 0, 255);
+    return rangeToUnit(toInt32(value), 0, 255);
 }
 
 /**
@@ -70,6 +105,200 @@ export function uByteToUnit(value: number): number {
  * @param b divisor
  */
 export function mod(a: number, b: number): number {
-    const r = a % b;
-    return r < 0 ? r + b : r === -0 ? 0 : r;
+    const r = maskMinus0(a % b);
+    return r < 0 ? r + b : r;
+}
+
+/**
+ * Divide operation that returns only the integer part of the quotient,
+ * such that _a = div(a, b) * b + mod(a, b)_.
+ * That is, the division result is rounded down.
+ *
+ * @example
+ *
+ * _divFloor(-13, 5) = -3_
+ * _divFloor(-10, 5) = -2_
+ * _divFloor(7, 5) = 1_
+ * _divFloor(15, 5) = 3_
+ *
+ * @param a dividend
+ * @param b divisor
+ */
+export function divFloor(a: number, b: number): number {
+    return maskMinus0(Math.floor(a / b));
+}
+
+/**
+ * Divide operation that returns only the integer part of the quotient,
+ * such that _a = div(a, b) * b + a % b_.
+ * That is, the division result is rounded towards zero.
+ *
+ * @example
+ *
+ * _divTrunc(-13, 5) = -2_
+ * _divTrunc(-10, 5) = -2_
+ * _divTrunc(7, 5) = 1_
+ * _divTrunc(15, 5) = 3_
+ *
+ * @param a dividend
+ * @param b divisor
+ */
+export function divTrunc(a: number, b: number): number {
+    return maskMinus0(Math.trunc(a / b));
+}
+
+/**
+ * Returns the fractional part, i.e., the decimal places of a value.
+ *
+ * @param value the value
+ */
+export function frac(value: number): number {
+    return value - Math.trunc(value);
+}
+
+/**
+ * Returns _true_, if a value is an integer, i.e., its fractional part is 0.
+ *
+ * @param value the value
+ */
+export function isInteger(value: number): boolean {
+    return frac(value) === 0;
+}
+
+/**
+ * Returns the smallest integer that is strictly greater than the given value.
+ *
+ * @param value the value
+ */
+export function higher(value: number) {
+    return isInteger(value) ? value + 1 : Math.ceil(value);
+}
+
+/**
+ * Returns the greatest integer that is strictly less than the given value.
+ *
+ * @param value the value
+ */
+export function lower(value: number) {
+    return isInteger(value) ? value - 1 : Math.floor(value);
+}
+
+/**
+ * Replaces _-0_ with _0_, and otherwise just returns _value_.
+ *
+ * @param value value that may be _-0_
+ */
+export function maskMinus0(value: number): number {
+    return value === -0 ? 0 : value;
+}
+
+/**
+ * Returns the sign of a value, but unlike _Math.sign()_, returns _0_ for both _-0_ and _0_.
+ *
+ * @param value the value
+ */
+export function signum(value: number): number {
+    return maskMinus0(Math.sign(value));
+}
+
+/**
+ * Converts a value into a 32 bit signed integer number.
+ *
+ * This behaves like cutting the off the decimal places of the number (instead of rounding),
+ * then taking the lower 32 bits of the result and interpreting them as a two's complement value.
+ * This is equivalent to _value ^ 0_, because in JavaScript, bitwise operations use 32 bit operands.
+ *
+ * @param value the value
+ */
+export function toInt32(value: number): number {
+    return value ^ 0;
+}
+
+/**
+ * Converts a value from radians into degrees.
+ *
+ * @param radians value in radians.
+ */
+export function radiansToDegrees(radians: number): number {
+    return (radians * 180) / Math.PI;
+}
+
+/**
+ * Converts a value from degrees into radians.
+ *
+ * @param degrees value in degrees.
+ */
+export function degreesToRadians(degrees: number): number {
+    return (degrees * Math.PI) / 180;
+}
+
+/**
+ * Maps a value, given in radians, into the modulus range _0_ (inclusive) to _2 * PI_ (exclusive).
+ *
+ * That is:
+ *
+ * - _-0.5 * PI_ is mapped to _1.5 * PI_
+ * - _2 * PI_ is mapped to _0_
+ * - _2.5 * PI_ is mapped to _0.5 * PI_
+ *
+ * @param radians
+ */
+export function radiansToDefaultRange(radians: number): number {
+    return mod(radians, 2 * Math.PI);
+}
+
+/**
+ * Maps a value, given in degrees, into the modulus range _0_ (inclusive) to _360_ (exclusive).
+ *
+ * That is:
+ *
+ * - _-90_ is mapped to _270_
+ * - _360_ is mapped to _0_
+ * - _450_ is mapped to _90_
+ *
+ * @param degrees
+ */
+export function degreesToDefaultRange(degrees: number): number {
+    return mod(degrees, 360);
+}
+
+/**
+ * Returns the _exponent_, such that _value = radix^exponent_.
+ *
+ * @param value _radix^exponent_
+ * @param radix _radix_
+ */
+export function log(value: number, radix: number): number {
+    return Math.log(value) / Math.log(radix);
+}
+
+/**
+ * Returns the _radix_, such that _value = radix^exponent_.
+ *
+ * @param value _radix^exponent_
+ * @param exponent _exponent_
+ */
+export function root(value: number, exponent: number): number {
+    return Math.exp(Math.log(value) / exponent);
+}
+
+/**
+ * Returns the number of digits required to represent the integral part of a value
+ * in a numbering system with the given radix.
+ *
+ * @param value the value
+ * @param radix radix of the numbering system
+ */
+export function getDigits(value: number, radix: number): number {
+    const adjustedValue = Math.max(Math.abs(value), 1);
+    return higher(log(adjustedValue, radix));
+}
+
+/**
+ * Returns the number of decimals required to represent the integral part of a value.
+ *
+ * @param value the value
+ */
+export function getDecimals(value: number): number {
+    return getDigits(value, 10);
 }
